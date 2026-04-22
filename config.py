af88446027719324a77fe6c904f024a326cbcad1
Chexchocco <88623042+Chexchocco@users.noreply.github.com>
2026-04-22 22:21:23 +0900

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-MODEL_NAME = 'my_sts_qwen'
-LOCAL_PATH  = r"C:\Users\MAIN\proejct_20200785\project"
-DB_PATH = os.path.join(LOCAL_PATH, "db", "parsed_item.json")
-LOG_PATH = os.path.join(LOCAL_PATH, "agent_log.txt")
